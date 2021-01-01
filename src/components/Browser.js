72efb75408b71d71bf4a20bd54e6308e3bac264a
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-01-01 11:21:10 -0500

    Updated readme

---
+++ src/components/Browser.js
@@ -0,0 +1,9 @@
+const Browser = () => {
+    return (         
+        <div class="two browser">
+            
+        </div>
+     );
+}
+ 
+export default Browser;
---
+++ src/components/Upload.js
@@ -0,0 +1,10 @@
+const Upload = () => {
+    return ( 
+        <div className="one">
+            <h1><strong>Hello,</strong> Safder</h1>
+            
+        </div>
+     );
+}
+ 
+export default Upload;
